9b3358e1c70e5bbd87514e43c34dd0faf7999315
morpig <pigoshoriginal1@gmail.com>
2013-08-09 19:52:38 +0700

    Forked from one of developer that's not in github!
    The developer: -COMING SOON- still need to work.

--- BasicPacMan/BasicPacMan/My Project/Application.Designer.vb
+++ BasicPacMan/BasicPacMan/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.BasicPacMan.Form1
+            Me.MainForm = Global.BasicPacMan.patmanMain
         End Sub
     End Class
 End Namespace
